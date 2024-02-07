54eb024f46a6431122596109069b0446b50d0dc0
Prince <prince.tiburcio.-nd@disney.com>
2024-02-08 04:29:03 +0800

    Update repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'shop', component: ShopComponent },
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'contact-us', component: ContactUsComponent },
+    { path: 'cart', component: CartComponent },
+];
